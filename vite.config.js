28d26403ca9e8d9cc2690521560f2c1583574fbe
KENMEGNE YOH IVANA MARINA <yohivana237@gmail.com>
2025-01-14 17:27:07 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    chunkSizeWarningLimit: 1000
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
-});
+})
